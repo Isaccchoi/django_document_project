681349fda4d4e0e93b08c83bdbb36db027aa69ee
isaccchoi <isaccchoi@naver.com>
2017-10-10 16:36:24 +0900

    many_to_many 모델들 패키지화

--- django_document/model/admin.py
+++ django_document/model/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from model.models import User, Car, Manufacturer
+from model.models import (
+    User,
+    Car,
+    Manufacturer,
+    Pizza,
+    Topping,
+    FacebookUser,
+    InstagramUser,
+)
 
 admin.site.register(User)
 admin.site.register(Car)
 admin.site.register(Manufacturer)
+admin.site.register(Pizza)
+admin.site.register(Topping)
+admin.site.register(FacebookUser)
+admin.site.register(InstagramUser)
---
+++ django_document/model/models/many_to_many/__init__.py
@@ -0,0 +1,4 @@
+from .basic import *
+from .self import *
+from .self_symmetrical_false import *
+
