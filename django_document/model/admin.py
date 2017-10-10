9fd3a7e37a2252c3f177acce18e73d2842851b2b
isaccchoi <isaccchoi@naver.com>
2017-10-10 17:19:52 +0900

    중간 모델을 이용한 ManyToMany연결

--- django_document/model/admin.py
+++ django_document/model/admin.py
@@ -3,12 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from model.models import (
     User,
-    Car,
-    Manufacturer,
-    Pizza,
-    Topping,
-    FacebookUser,
-    InstagramUser,
+    Car, Manufacturer,
+    Pizza, Topping,
+    FacebookUser, InstagramUser,
+    Idol, Group, Membership,
 )
 
 admin.site.register(User)
@@ -18,3 +16,6 @@ admin.site.register(Pizza)
 admin.site.register(Topping)
 admin.site.register(FacebookUser)
 admin.site.register(InstagramUser)
+admin.site.register(Idol)
+admin.site.register(Group)
+admin.site.register(Membership)
