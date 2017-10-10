094c3629e76788c0862da2f9254ae991cb6413d1
isaccchoi <isaccchoi@naver.com>
2017-10-10 14:07:48 +0900

    User 모델 추가

--- django_document/model/models.py
+++ django_document/model/models.py
@@ -15,10 +15,24 @@ class Fruit(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
 
 
+class Car(models.Model):
+    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.manufacturer.name} - {self.name}'
+
+
 class Manufacturer(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
 
-class Car(models.Model):
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+
+class User(models.Model):
     name = models.CharField(max_length=30)
+    teacher = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
