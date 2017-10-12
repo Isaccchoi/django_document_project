7ba821ec034998dde94da391102899d53a7138a8
isaccchoi <isaccchoi@naver.com>
2017-10-12 15:34:57 +0900

    PostgreSQL로 db변경

--- django_document/django_document/settings.py
+++ django_document/django_document/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'django_document.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'doc',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'isaccchoi',
+        'PASSWORD': 'asdfqwer1234',
     }
 }
 
